74ae3917711ebd150be6a364aa68ba5d93ea038f
Donovan Clay <donovan@donovanclay.com>
2021-09-13 23:33:12 -0700

    Comment-out the template node.

--- nodes/__init__.py
+++ nodes/__init__.py
@@ -1,6 +1,6 @@
 
 """ Node classes used by the Python template Node Server. """
 
-from .TemplateNode            import TemplateNode
+#from .TemplateNode            import TemplateNode
 from .IAQNode                 import IAQNode
 from .TemplateController      import TemplateController
